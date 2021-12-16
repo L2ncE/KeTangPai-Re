8d5c83883e32b44abf65d0dd6ac3b3685415ccf8
L2ncE <llance_24@foxmail.com>
2021-12-16 20:11:09 +0800

    feat(service):新增对用户相关服务的注释

--- service/user.go
+++ service/user.go
@@ -6,11 +6,13 @@ import (
 	"message-board/model"
 )
 
+// ChangePassword 修改密码服务
 func ChangePassword(username, newPassword string) error {
 	err := dao.UpdatePassword(username, newPassword)
 	return err
 }
 
+// IsPasswordCorrect 判断密码是否正确服务
 func IsPasswordCorrect(username, password string) (bool, error) {
 	user, err := dao.SelectUserByUsername(username)
 	if err != nil {
@@ -42,11 +44,13 @@ func IsRepeatUsername(username string) (bool, error) {
 	return true, nil
 }
 
+// Register 注册服务
 func Register(user model.User) error {
 	err := dao.Insert(user)
 	return err
 }
 
+// SelectAnswerByUsername 通过昵称查找答案服务
 func SelectAnswerByUsername(username string) string {
 	answer := dao.SelectAnswerByUsername(username)
 	return answer
