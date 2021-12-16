da9754627e61ca399632cbbffac9f114a671903c
L2ncE <llance_24@foxmail.com>
2021-12-16 20:44:57 +0800

    feat(model):新增话题model

---
+++ model/topic.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type Topic struct {
+	Id         int       `json:"id"`
+	Context    string    `json:"context"`
+	Name       string    `json:"name"`
+	PostTime   time.Time `json:"post_time"`
+	UpdateTime time.Time `json:"update_time"`
+}
