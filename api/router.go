592d042af527ba7da55abe8893a55acb3cf3dcc8
L2ncE <llance_24@foxmail.com>
2021-12-16 18:27:21 +0800

    feat(api):新增路由中注册登录

--- api/router.go
+++ api/router.go
@@ -7,6 +7,9 @@ import (
 func InitEngine() {
 	engine := gin.Default()
 
+	engine.POST("/register", register) //注册
+	engine.POST("/login", login)       //登陆
+
 	err := engine.Run()
 	if err != nil {
 		return
