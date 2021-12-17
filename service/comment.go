3dbda0db4fd6e0284493808e780388234253448f
L2ncE <llance_24@foxmail.com>
2021-12-17 09:09:13 +0800

    feat(service):新增话题评论服务

---
+++ service/comment.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"ketangpai/dao"
+	"ketangpai/model"
+)
+
+func AddComment(comment model.Comment) error {
+	return dao.InsertComment(comment)
+}
+
+func GetPostComments(postId int) ([]model.Comment, error) {
+	return dao.SelectCommentByPostId(postId)
+}
+
+func DeleteComment(Id int) error {
+	err := dao.DeleteComment(Id)
+	return err
+}
+
+func GetNameById2(commentId int) (string, error) {
+	return dao.SelectNameById2(commentId)
+}
+
+func CommentLikes(commentId int) error {
+	err := dao.CommentLikes(commentId)
+	return err
+}
