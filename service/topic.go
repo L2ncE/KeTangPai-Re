7ab5f3e1702ccf337831cc5173bb12464fd8f75d
L2ncE <llance_24@foxmail.com>
2021-12-18 17:34:58 +0800

    refactor(service):更新服务相关注释

--- service/topic.go
+++ service/topic.go
@@ -6,33 +6,40 @@ import (
 	"time"
 )
 
+// AddTopic 添加话题
 func AddTopic(topic model.Topic) error {
 	err := dao.InsertTopic(topic)
 	return err
 }
 
+// DeleteTopic 删除话题
 func DeleteTopic(topicId int) error {
 	err := dao.DeleteTopic(topicId)
 	return err
 }
 
+// GetTopics 得到话题
 func GetTopics() ([]model.Topic, error) {
 	return dao.SelectTopic()
 }
 
+// GetTopicById 通过id得到话题
 func GetTopicById(topicId int) (model.Topic, error) {
 	return dao.SelectTopicById(topicId)
 }
 
+// GetNameById 通过id拿到用户名
 func GetNameById(topicId int) (string, error) {
 	return dao.SelectNameById(topicId)
 }
 
+// ChangeTopic 改变话题
 func ChangeTopic(id int, context string, UpdateTime time.Time) error {
 	err := dao.ChangeTopic(id, context, UpdateTime)
 	return err
 }
 
+// TopicLikes 话题点赞
 func TopicLikes(topicId int) error {
 	err := dao.TopicLikes(topicId)
 	return err
