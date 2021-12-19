73980d7baabd9b69f9a54e92cc757eadaf847039
L2ncE <llance_24@foxmail.com>
2021-12-19 18:06:32 +0800

    refactor(api):更改下载文件回复位置

--- api/download.go
+++ api/download.go
@@ -15,9 +15,9 @@ func download(ctx *gin.Context) {
 		tool.RespInternalError(ctx)
 		return
 	}
+	tool.RespSuccessful(ctx)
 	ctx.Header("Content-Type", "application/octet-stream")              //常见的文件下载格式
 	ctx.Header("Content-Disposition", "attachment; filename="+fileName) //下载到本地
 	ctx.Header("Content-Transfer-Encoding", "binary")
 	ctx.File(fileDir + "/" + fileName)
-	tool.RespSuccessful(ctx)
 }
